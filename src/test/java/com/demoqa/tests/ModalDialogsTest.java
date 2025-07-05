7b24a46321dc753d3bcc4f5cf84f735af0761c7d
Boris Pechersky <bpechersky@gmx.com>
2025-07-05 16:39:13 -0700

    updated failing in GitHub Actions  public void testLargeModal() {

--- src/test/java/com/demoqa/tests/ModalDialogsTest.java
+++ src/test/java/com/demoqa/tests/ModalDialogsTest.java
@@ -2,8 +2,16 @@ package com.demoqa.tests;
 
 import com.demoqa.BaseTest;
 import com.demoqa.pages.ModalDialogsPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
 
 public class ModalDialogsTest extends BaseTest {
 
@@ -13,19 +21,26 @@ public class ModalDialogsTest extends BaseTest {
         page.openPage();
 
         page.clickSmallModalButton();
-        Assert.assertEquals(page.getSmallModalTitle(), "Small Modal");
+        assertEquals(page.getSmallModalTitle(), "Small Modal");
 
         page.closeSmallModal();
     }
 
     @Test
     public void testLargeModal() {
+        WebDriver driver = getDriver();
         ModalDialogsPage page = new ModalDialogsPage(driver);
         page.openPage();
 
         page.clickLargeModalButton();
-        Assert.assertEquals(page.getLargeModalTitle(), "Large Modal");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("example-modal-sizes-title-lg")));
+
+        String modalTitle = page.getLargeModalTitle();
+        assertEquals(modalTitle, "Large Modal");
 
         page.closeLargeModal();
     }
+
 }
